3218f2b6983224e4d3cf5479816b7ba39b9ced9a
GONZOLOL <glerdie489@g.educaand.es>
2023-01-31 14:59:46 +0100

    header component creation

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 
 function Header() {
     return (
-        <header className="navBar-header">
-            <img src="/images/logo.svg" alt="" />
-            <span>hola</span>
-            <span></span>
-            <span></span>
-        </header>
+        <>
+            <header className='navBar-header'>
+                <div className="navBar-header-left">
+                    <img src="/images/logo.svg" alt="" />
+                    <a>Features</a>
+                    <a>Pricing</a>
+                    <a>Resources</a>
+                </div>
+                <div className="navBar-header-right">
+                    <a>login</a>
+                    <a>signUp</a>
+                </div>
+            </header>
+        </>
     );
   }
   
