9a177e445015f2b5b97d88833ae2f7ccf416875b
GONZOLOL <glerdie489@g.educaand.es>
2023-01-31 11:57:09 +0100

    sass

---
+++ src/header.jsx
@@ -0,0 +1,17 @@
+import './header.scss';
+import React from 'react';
+
+function Header() {
+    return (
+        <header className="navBar-header">
+            <img src="../public/images/logo.svg" alt="" />
+            <span></span>
+            <span></span>
+            <span></span>
+        </header>
+    );
+  }
+  
+  export default Header;
+  
+
