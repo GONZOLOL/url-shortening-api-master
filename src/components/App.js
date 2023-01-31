58ca54c04162bf45ba4d8b4e71a52c77fd1181af
GONZOLOL <glerdie489@g.educaand.es>
2023-01-31 13:00:19 +0100

    cambios

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,3 @@
-import './App.scss';
 import React from 'react';
 
 function App() {
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,10 @@
-import './header.scss';
 import React from 'react';
 
 function Header() {
     return (
         <header className="navBar-header">
-            <img src="../public/images/logo.svg" alt="" />
-            <span></span>
+            <img src="/images/logo.svg" alt="" />
+            <span>hola</span>
             <span></span>
             <span></span>
         </header>
