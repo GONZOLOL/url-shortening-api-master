78537981e62b0617f326910aa62b7a2c5a093a79
GONZOLOL <glerdie489@g.educaand.es>
2023-02-09 12:41:26 +0100

    main component changes

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './styles/index.scss';
 import App from './components/App';
 import Header from './components/Header';
+import Body from './components/Body';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header />
     <App />
+    <Header />
+    <Body/>
   </React.StrictMode>
 );
