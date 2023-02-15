c59d87518dec09395a55d4dd6a8608a595e5b11b
GONZOLOL <glerdie489@g.educaand.es>
2023-02-15 21:09:25 +0100

    Result Cards creation and styling

---
+++ src/components/ResultCard.jsx
@@ -0,0 +1,17 @@
+import Button from './Button';
+
+export function ResultCard({ originalLink, shortLink }) {
+
+    const handleCopy = (event) => {
+        const value = event.target.dataset.shortlink;
+        navigator.clipboard.writeText(value);
+    }
+
+    return (
+        <div className='shortedUrlWrapper'>
+            <span className='originalUrl'>{originalLink}</span>
+            <a className='shortedUrl' href={shortLink} target="_blank" rel="noreferrer">{shortLink}</a>
+            <Button dataShortLink={shortLink} estilo="btn btn--input" text="Copy" childClick={handleCopy} />
+        </div>   
+    )
+}
