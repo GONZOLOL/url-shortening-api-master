61308e5d2db57fc047e7ed68437b6ad8f4b4d826
GONZOLOL <glerdie489@g.educaand.es>
2023-02-19 16:32:37 +0100

    button copied style added

--- src/components/ResultCard.jsx
+++ src/components/ResultCard.jsx
@@ -2,16 +2,26 @@ import Button from './Button';
 
 export function ResultCard({ originalLink, shortLink }) {
 
+    const copy = document.querySelectorAll('.btn--result--card');
+    
     const handleCopy = (event) => {
         const value = event.target.dataset.shortlink;
         navigator.clipboard.writeText(value);
+        
+        copy.forEach(y => {
+            y.classList.remove('copied');
+            y.innerHTML = "Copy";
+        })
+        event.target.innerHTML = "Copied!";
+        event.target.className = "btn--result--card copied";
+
     }
 
     return (
         <div className='shortedUrlWrapper'>
             <span className='originalUrl'>{originalLink}</span>
             <a className='shortedUrl' href={shortLink} target="_blank" rel="noreferrer">{shortLink}</a>
-            <Button dataShortLink={shortLink} estilo="btn btn--input" text="Copy" childClick={handleCopy} />
+            <Button id="copyButton" dataShortLink={shortLink} estilo="btn--result--card" text="Copy" childClick={handleCopy}/>
         </div>
     )
 }
